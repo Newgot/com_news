7adf8fc85bd1f280a823c7315433c31e5fe84f86
Vladistal Bakhov <kreazilist@gmail.com>
2021-04-21 16:47:42 +0700

    Реадиктирование Create database

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -1,11 +1,9 @@
-DROP TABLE IF EXISTS `#__news`;
-
 CREATE TABLE IF NOT EXISTS `#__news` (
     `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `title` varchar(25) NOT NULL,
     `published` tinyint,
     `text` text NOT NULL,
-    `images` vachar(1024) NOT NULL DEFAULT '',
+    `images` varchar(1024) NOT NULL DEFAULT '',
     `publish_up` datetime,
     `alias` varchar(45)
-)
+    )
