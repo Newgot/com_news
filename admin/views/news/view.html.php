de8298292d656c5a6f2ea44640646493479bbfa0
Vladistal Bakhov <kreazilist@gmail.com>
2021-04-22 10:42:50 +0700

    Формироваие точки входа

--- admin/views/news/view.html.php
+++ admin/views/news/view.html.php
@@ -2,10 +2,33 @@
 
 defined('_JEXEC') or exit();
 
-class NewsViewNews extends  JViewLegacy {
+class NewsViewNews extends JViewLegacy
+{
 
-    public function display($tpl='') {
-
+    public function display($tpl = '')
+    {
+        $this->addToolBar();
         parent::display();
+        $this->setDocument();
+    }
+
+    protected function addToolBar()
+    {
+        JToolBarHelper::title(JText::_('COM_NEWS_MANAGER_NEWS'));
+
+        JToolBarHelper::addNew('item.add');
+        JToolBarHelper::editList('item.edit');
+        JToolBarHelper::deleteList('', 'news.delete');
+
+    }
+
+    protected function setDocument()
+    {
+        $document = JFactory::getDocument();
+        $document->setTitle(JText::_('COM_NEWS_ADMINISTRATION'));
+        echo '<pre>';
+        var_dump($document);
+        echo '</pre>';
+
     }
 }
